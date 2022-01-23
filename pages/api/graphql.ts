698beac9ba0a9eb74532de6982df8cecaa2e37ce
chrisbolosan <cbolosan@gmail.com>
2022-01-23 17:18:43 -0600

    graphlql api route up

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,38 +4,57 @@ import { IResolvers } from "@graphql-tools/utils";
 import type { NextApiHandler } from "next";
 
 const typeDefs = gql`
-enum TaskStatus{
-  active
-  completed
-}
-  type Query {
-  id: Int!
-  title: String!
-  status: TaskStatus!
-}
-input CreateTaskInput {
-  title: String!
-}
-input UpdateTaskInput {
-  id: Int!
-  title: String
-  status: TaskStatus
-}
-type Query {
-  tasks(status: TaskStatus): [Task!]!
-  task(id: Int!): Task
-}
+  enum TaskStatus {
+    active
+    completed
   }
+
+  type Task {
+    id: Int!
+    title: String!
+    status: TaskStatus!
+  }
+
+  input CreateTaskInput {
+    title: String!
+  }
+
+  input UpdateTaskInput {
+    id: Int!
+    title: String
+    status: TaskStatus
+  }
+
+  type Query {
+    tasks(status: TaskStatus): [Task!]!
+    task(id: Int!): Task
+  }
+
   type Mutation {
     createTask(input: CreateTaskInput!): Task
     updateTask(input: UpdateTaskInput!): Task
+    deleteTask(id: Int!): Task
   }
 `;
 
 const resolvers: IResolvers = {
   Query: {
-    users(parent, args, context) {
-      return [{ name: "Nextjs" }];
+    tasks(parent, args, context) {
+      return [];
+    },
+    task(parent, args, context) {
+      return null;
+    },
+  },
+  Mutation: {
+    createTask(parent, args, context) {
+      return null;
+    },
+    updateTask(parent, args, context) {
+      return null;
+    },
+    deleteTask(parent, args, context) {
+      return null;
     },
   },
 };
